f2b87088f4f3c3aa01ac5e939498480001701c87
padsley <padsley@gmail.com>
2019-03-15 19:07:16 +0000

    Add shell scripts for doing AngCor calculations

---
+++ DoAngCorForQuadState.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#This code runs the AngCor calculations from start to finish for the quadrupole state
+
+#CHUCK3 calculations
+
+
+#AngCor inputs generation
+
+
+#AngCor calculations
+
+
+#Creation of the combined AngCor file
+
+
+#Monte Carlo for the quadrupole state
